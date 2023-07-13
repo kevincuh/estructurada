09c5f44eb5f9ebb8b6b9a8604f3f73e4ccb3ecc8
AlexOf99 <ordazfragosoalexrobinson12@gmail.com>
2023-07-13 10:23:14 -0600

    funcion suma

--- Main.c
+++ Main.c
@@ -4,9 +4,13 @@
 main()
 {
 	int n1, n2, n3;
+	int suma;
 	printf("Ingresa n1: ");
 	scanf("%d",&n1);
 	printf("Ingresa n2: ");
 	scanf("%d",&n2);
+	//alex ordaz fragoso
+	suma = n1 + n2;
 	printf("n1 = %d  n2 = %d",n1,n2);
+	pintd("\nLa suma es %d",suma);
 }
