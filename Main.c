9c543a21a39b1da053e6aa908dcc9faa0c1c1cb2
Yoshy0777 <139258576+Yoshy0777@users.noreply.github.com>
2023-07-13 10:36:33 -0600

    Main.c

--- Main.c
+++ Main.c
@@ -4,13 +4,14 @@
 main()
 {
 	int n1, n2, n3;
-	int suma;
+	//Yoshy
+	int resta;
 	printf("Ingresa n1: ");
 	scanf("%d",&n1);
 	printf("Ingresa n2: ");
 	scanf("%d",&n2);
-	//alex ordaz fragoso
-	suma = n1 + n2;
+
+	resta = n1 + n2;
 	printf("n1 = %d  n2 = %d",n1,n2);
-	pintd("\nLa suma es %d",suma);
+	pintd("\nLa resta es %d",resta);
 }
