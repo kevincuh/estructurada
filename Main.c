16b58bf9e9c2ac094b165bff97c912ac9bc8a2d1
kevincuh <kevin.jaramillo@cuh.mx>
2023-07-13 10:53:49 -0600

    Correccion de signo menos

--- Main.c
+++ Main.c
@@ -4,9 +4,14 @@
 main()
 {
 	int n1, n2, n3;
+	//Yoshy
+	int resta;
 	printf("Ingresa n1: ");
 	scanf("%d",&n1);
 	printf("Ingresa n2: ");
 	scanf("%d",&n2);
+
+	resta = n1 - n2;
 	printf("n1 = %d  n2 = %d",n1,n2);
+	pintd("\nLa resta es %d",resta);
 }
